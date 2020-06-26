c291a4ecbb6c285873bda04ed6dbaad47efd8fc6
paulo.esantos <paulo.esantos@gmail.com>
2020-06-26 19:53:06 -0300

    Cap-06 6.7

--- componentes/DimensoesFixas.js
+++ componentes/DimensoesFixas.js
@@ -3,17 +3,12 @@ import { View } from 'react-native';
     class DimensoesFixas extends React.Component {
     render() {
         return (
-                <View style={{width: '100%', height: '100%', flexDirection: 'column', flexWrap: 'wrap'}}>
-                    <View style={{width: 50, height: 100, backgroundColor: 'powderblue'}} />
-                    <View style={{width: 50, height: 100, backgroundColor: 'skyblue'}} />
-                    <View style={{width: 50, height: 100, backgroundColor: 'steelblue'}} />
-                    <View style={{width: 50, height: 100, backgroundColor: 'powderblue'}} />
-                    <View style={{width: 50, height: 100, backgroundColor: 'skyblue'}} />
-                    <View style={{width: 50, height: 100, backgroundColor: 'steelblue'}} />
-                    <View style={{width: 50, height: 100, backgroundColor: 'powderblue'}} />
-                    <View style={{width: 50, height: 100, backgroundColor: 'skyblue'}} />
-                    <View style={{width: 50, height: 100, backgroundColor: 'steelblue'}} />
-                </View>
+            <View style={{width: '100%', height: '100%', flexDirection: 'column', justifyContent: 'center', alignItems: 'stretch'}}>
+                <View style={{flexGrow: 1, backgroundColor: 'powderblue'}} />
+                <View style={{flexGrow: 2, backgroundColor: 'skyblue'}} />
+                <View style={{flexGrow: 1,backgroundColor: 'steelblue'}} />
+            </View>
+           
         );
     }
 };
