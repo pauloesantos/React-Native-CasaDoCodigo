01ec19180743ad1c2f435de3df4b7069add2765b
paulo.esantos <paulo.esantos@gmail.com>
2020-06-26 19:54:45 -0300

    Cap-06 6.8

--- componentes/DimensoesFixas.js
+++ componentes/DimensoesFixas.js
@@ -4,10 +4,11 @@ import { View } from 'react-native';
     render() {
         return (
             <View style={{width: '100%', height: '100%', flexDirection: 'column', justifyContent: 'center', alignItems: 'stretch'}}>
-                <View style={{flexGrow: 1, backgroundColor: 'powderblue'}} />
-                <View style={{flexGrow: 2, backgroundColor: 'skyblue'}} />
-                <View style={{flexGrow: 1,backgroundColor: 'steelblue'}} />
+                <View style={{flexShinrk: 1, flexBasis: 300, backgroundColor: 'powderblue'}} />
+                <View style={{flexShrink: 3, flexBasis: 300, backgroundColor: 'skyblue'}} />
+                <View style={{flexShrink: 1, flexBasis: 300, backgroundColor: 'steelblue'}} />
             </View>
+
            
         );
     }
