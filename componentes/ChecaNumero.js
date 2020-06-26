ca779d199e8c922a1b9b664c740ba3583cb1eca3
paulo.esantos <paulo.esantos@gmail.com>
2020-06-26 20:17:30 -0300

    Cap-7 2

--- componentes/ChecaNumero.js
+++ componentes/ChecaNumero.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default props =>
-<View>
-    {
-        props.numero % 2 == 0
+
+<View style={styles.container}>
+    {validaParOuImpar(props.numero)}
+ </View>
+
+
+function validaParOuImpar(numero) {
+    return numero % 2 == 0
         ? <Text style={styles.text}>O número é par!</Text>
         : <Text style={styles.text}>O número é ímpar!</Text>
     }
-</View>
+
 const styles = StyleSheet.create({
     container: {
             flex: 1,
